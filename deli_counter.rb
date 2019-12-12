b98e911cf979c4fd78bab6742c19d46a1e3aabad
Tammy Lam <lamtammy1515@gmail.com>
2019-12-12 18:29:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
+require 'pry'
+
+
 # Write your code here.
+ katz_deli = []
+ 
+ function take_a_number(katz_deli, name)
+binding.pry
+katz_deli.push(name)
+return "Welcome, " + name + ". You are number " + katz_deli.length + " in line."
+
+
+
+
+
+
+function now_serving(katz_deli)
+if (katz_deli.length > 0) 
+return "Currently serving " + katz_deli.shift() + ".""
+else
+return "There is nobody waiting to be served!"
+
+
+    
+  end
